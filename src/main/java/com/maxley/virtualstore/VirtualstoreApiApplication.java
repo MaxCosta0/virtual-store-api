e07adbe82e434cdd54159f4e3becdabce97071a3
MaxCosta0 <maxleysoares006@gmail.com>
2021-01-25 06:20:42 -0300

    State and City models implementation

--- src/main/java/com/maxley/virtualstore/VirtualstoreApiApplication.java
+++ src/main/java/com/maxley/virtualstore/VirtualstoreApiApplication.java
@@ -8,9 +8,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.maxley.virtualstore.domain.Category;
+import com.maxley.virtualstore.domain.City;
 import com.maxley.virtualstore.domain.Product;
+import com.maxley.virtualstore.domain.State;
 import com.maxley.virtualstore.repository.CategoryRepository;
+import com.maxley.virtualstore.repository.CityRepository;
 import com.maxley.virtualstore.repository.ProductRepository;
+import com.maxley.virtualstore.repository.StateRepository;
 
 @SpringBootApplication
 public class VirtualstoreApiApplication implements CommandLineRunner {
@@ -20,6 +24,12 @@ public class VirtualstoreApiApplication implements CommandLineRunner {
 	
 	@Autowired
 	private ProductRepository productRepository;
+	
+	@Autowired
+	private StateRepository stateRepository;
+	
+	@Autowired
+	private CityRepository cityRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(VirtualstoreApiApplication.class, args);
@@ -43,6 +53,23 @@ public class VirtualstoreApiApplication implements CommandLineRunner {
 		
 		categoryRepository.saveAll(Arrays.asList(category1, category2));	
 		productRepository.saveAll(Arrays.asList(product1, product2, product3));
+		
+		State state1 = new State(null, "Minas Gerais");
+		State state2 = new State(null, "Sao Paulo");
+		
+		City city1 = new City(null, "Uberlandia", state1);
+		City city2 = new City(null, "Sao Paulo", state2);
+		City city3 = new City(null, "Campinas", state2);
+		
+		//state1.getCities().addAll(Arrays.asList(city1));
+		//state2.getCities().addAll(Arrays.asList(city2, city3));
+		
+		state1.setCities(Arrays.asList(city1));
+		state2.setCities(Arrays.asList(city2, city3));
+		
+		stateRepository.saveAll(Arrays.asList(state1, state2));
+		cityRepository.saveAll(Arrays.asList(city1, city2, city3));
+		
 	}
 
 }
